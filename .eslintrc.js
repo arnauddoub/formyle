4d29a9f485bd6a17d27122df6d6bab3d6d35406b
ArnaudDoubremelle <arnaud.doub@gmail.com>
2021-02-06 23:47:38 +0100

    fix format

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,34 +4,33 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    "plugin:vue/vue3-recommended",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
+  extends: ['plugin:vue/vue3-recommended', 'eslint:recommended', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
   rules: {
-    'prettier/prettier': ['error', {
-      'singleQuote': true,
-      'semi': false,
-      'tabWidth': 2,
-      'trailingComma': 'all',
-      'printWidth': 120,
-    }],
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: false,
+        tabWidth: 2,
+        trailingComma: 'all',
+        printWidth: 120,
+      },
+    ],
     'vue/html-self-closing': [
       'error',
       {
-        'html': {
-          'void': 'any',
+        html: {
+          void: 'any',
         },
       },
     ],
-    'indent': ['error', 2],
-    'quotes': ['error', 'single'],
-    'semi': ['error', 'never'],
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
     'comma-dangle': ['error', 'always-multiline'],
     'object-curly-spacing': ['error', 'always'],
     'vue/max-attributes-per-line': 'off',
