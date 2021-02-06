9e28e647c819833c695651415401f2681bdaaea5
ArnaudDoubremelle <arnaud.doub@gmail.com>
2021-02-06 23:47:20 +0100

    add Form route

--- src/router.js
+++ src/router.js
@@ -1,16 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { STEPS } from './views/Form/steps'
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: () => import(/* webpackChunkName: "home" */ './views/Home.vue'),
+    path: '/form',
+    name: 'Form',
+    component: () => import(/* webpackChunkName: "form" */ './views/Form/Form.vue'),
+    redirect: { name: 'Loans' },
+    children: STEPS,
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: { name: 'Form' },
   },
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+})
+
+router.afterEach((to, from) => {
+  const toDepth = STEPS.findIndex((element) => element.name === to.name)
+  const fromDepth = STEPS.findIndex((element) => element.name === from.name)
+  to.meta.transitionName = toDepth < fromDepth ? 'page-left' : 'page-right'
 })
 
 export default router
