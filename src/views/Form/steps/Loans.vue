73d7a36a9eb122316c47bb3b0eea8148303c223b
ArnaudDoubremelle <arnaud.doub@gmail.com>
2021-02-06 23:44:47 +0100

    add Steps example

---
+++ src/views/Form/steps/Loans.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="loan">
+    <fields>
+      <field label="Montant du prêt">
+        <input-numeric v-model="value" :allow-decimal="true" />
+      </field>
+      <field label="Taux de prêt">
+        <inputa />
+      </field>
+    </fields>
+    <field label="Durée totale">
+      <inputa />
+    </field>
+  </div>
+</template>
+
+<script>
+import InputNumeric from '../../../components/InputNumeric.vue'
+import Field from '../../../components/Field.vue'
+import Fields from '../../../components/Fields.vue'
+import Inputa from '../../../components/Inputa.vue'
+
+export default {
+  name: 'Loan',
+  components: {
+    Inputa,
+    Fields,
+    Field,
+    InputNumeric,
+  },
+
+  data() {
+    return {
+      value: 100,
+    }
+  },
+
+  methods: {
+    getValue() {
+      console.log('click', this.value, typeof this.value)
+    },
+  },
+}
+</script>
---
+++ src/views/Form/steps/index.js
@@ -0,0 +1,15 @@
+import Loans from './Loans.vue'
+import Details from './Details.vue'
+
+export const STEPS = [
+  {
+    name: 'Loans',
+    path: 'loans',
+    component: Loans,
+  },
+  {
+    name: 'Details',
+    path: 'details',
+    component: Details,
+  },
+]
