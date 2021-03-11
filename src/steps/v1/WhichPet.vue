def527f95f0c0d2cfabca39f2e49d9b824a65c96
ArnaudDoubremelle <arnaud.doub@gmail.com>
2021-03-11 20:23:12 +0100

    add pets image

--- src/steps/v1/WhichPet.vue
+++ src/steps/v1/WhichPet.vue
@@ -3,13 +3,34 @@
     <heading :size="3">Quel animal souhaitez-vous assurer ?</heading>
     <field type="radio">
       <radio-group>
-        <radio-button id="witch_pet-chien" v-model="witchPet" name="witch_pet" value="dog" @change="$emit('nextStep')">
-          Chien
+        <radio-button
+          id="witch_pet-chien"
+          v-model="witchPet"
+          name="witch_pet"
+          value="dog"
+          :error="v$.witchPet.$error"
+          @change="$emit('nextStep')"
+        >
+          <div class="text-center">
+            <img src="../../assets/dog.svg" alt="dog" width="60" height="60" />
+            <div class="mt-2">Chien</div>
+          </div>
         </radio-button>
-        <radio-button id="witch_pet-chat" v-model="witchPet" name="witch_pet" value="cat" @change="$emit('nextStep')">
-          Chat
+        <radio-button
+          id="witch_pet-chat"
+          v-model="witchPet"
+          name="witch_pet"
+          value="cat"
+          :error="v$.witchPet.$error"
+          @change="$emit('nextStep')"
+        >
+          <img src="../../assets/cat.svg" alt="cat" width="60" height="60" />
+          <div class="mt-2">Chat</div>
         </radio-button>
       </radio-group>
+      <template v-if="v$.witchPet.$error" #error>
+        <span v-if="v$.witchPet.required">Veuillez sélectionner votre animal à assurer</span>
+      </template>
     </field>
   </div>
 </template>
