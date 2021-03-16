f5414a5f38a58f345f19bb6f1adb41dc10c8c03f
ArnaudDoubremelle <arnaud.doub@gmail.com>
2021-03-16 23:01:26 +0100

    add Selecta component

---
+++ src/components/Selecta.vue
@@ -0,0 +1,39 @@
+<template>
+  <select :id="id" v-model="displayValue" :name="name" :class="{ active: displayValue }">
+    <slot />
+  </select>
+</template>
+
+<script>
+import { computed } from 'vue'
+
+export default {
+  props: {
+    modelValue: {
+      type: [String, Number],
+      default: null,
+    },
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+
+  emits: ['update:modelValue'],
+
+  setup(props, { emit }) {
+    const displayValue = computed({
+      get: () => props.modelValue,
+      set: (value) => emit('update:modelValue', value),
+    })
+
+    return {
+      displayValue,
+    }
+  },
+}
+</script>
