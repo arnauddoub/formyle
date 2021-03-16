b210943cff35c8501c1546777aa7088f2a0f03ce
ArnaudDoubremelle <arnaud.doub@gmail.com>
2021-03-16 23:01:53 +0100

    add data for step

---
+++ src/steps/v1/data.js
@@ -0,0 +1,25 @@
+export const breeds = {
+  0: 'Rottweiler',
+  1: 'Berger allemand',
+  2: 'Golden Retriever',
+  3: 'Carlin',
+  4: 'Labrador Retriever',
+  5: 'Bouledogue Français',
+  6: 'Beagle',
+  7: 'Caniche',
+  8: 'Yorkshire Terrier',
+  9: 'Cane Corso',
+}
+
+export const years = {
+  0: 'Moins d\'un an',
+  1: '1 ans',
+  2: '2 ans',
+  3: '3 ans',
+  4: '4 ans',
+  5: '5 ans',
+  6: '6 ans',
+  7: '7 ans',
+  8: '8 ans',
+  9: '9 ans',
+}
