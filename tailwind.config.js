182a3c24cc4a56bc901be63b2efcebc65d8dd8d0
ArnaudDoubremelle <arnaud.doub@gmail.com>
2021-03-19 11:35:29 +0100

    remove unused variable

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
