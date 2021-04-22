5b3e494a0a53ba3a7f41bcbc845d01c25a36e60e
ArnaudDoubremelle <arnaud.doub@gmail.com>
2021-04-22 15:21:14 +0200

    :lipstick: update: Field component

--- src/components/Field.vue
+++ src/components/Field.vue
@@ -4,7 +4,7 @@
       <div class="block text-gray-500 leading-none mb-2">{{ label }}</div>
       <slot />
     </div>
-    <div v-else class="relative border-2 rounded-md focus-within:border-alpha">
+    <div v-else class="relative border border-gray-300 rounded-md focus-within:border-alpha">
       <slot />
       <label v-if="label" class="absolute top-0 bg-white px-4 py-3 -z-1 duration-300 origin-0 text-gray-500">
         {{ label }}
