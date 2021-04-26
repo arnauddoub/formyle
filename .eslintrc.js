e6a0bf4686507ed58530b95bdff5839590491478
ArnaudDoubremelle <arnaud.doub@gmail.com>
2021-04-26 16:26:10 +0200

    :wrench:	 config: update config eslint + prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,38 +4,13 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['plugin:vue/vue3-recommended', 'eslint:recommended', 'plugin:prettier/recommended'],
+  extends: ['plugin:vue/vue3-recommended', 'eslint:recommended', 'prettier'],
+  plugins: ['prettier'],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
   rules: {
-    'prettier/prettier': [
-      'error',
-      {
-        singleQuote: true,
-        semi: false,
-        tabWidth: 2,
-        trailingComma: 'all',
-        printWidth: 120,
-      },
-    ],
-    'vue/html-self-closing': [
-      'error',
-      {
-        html: {
-          void: 'any',
-        },
-      },
-    ],
-    indent: ['error', 2],
-    quotes: ['error', 'single', { avoidEscape: true }],
-    semi: ['error', 'never'],
-    'comma-dangle': ['error', 'always-multiline'],
-    'object-curly-spacing': ['error', 'always'],
-    'vue/max-attributes-per-line': 'off',
-    'vue/multiline-html-element-content-newline': 'off',
-    'vue/singleline-html-element-content-newline': 'off',
-    'no-useless-escape': 'error',
+    'prettier/prettier': 'error',
   },
 }
