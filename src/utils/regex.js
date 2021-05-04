5316fbc99edcbfa2be4bf6d78228974a771a3fd4
ArnaudDoubremelle <arnaud.doub@gmail.com>
2021-05-04 19:17:10 +0200

    test: add test for regex utils

--- src/utils/regex.js
+++ src/utils/regex.js
@@ -1,7 +1,15 @@
+/**
+ * @param {string} str
+ * @returns {string}
+ */
 export function stripSpace(str) {
   return str.replace(/\s/g, '')
 }
 
+/**
+ * @param {string} str
+ * @returns {string}
+ */
 export function stripNaN(str) {
   return str.replace(/[^\d]/g, '')
 }
