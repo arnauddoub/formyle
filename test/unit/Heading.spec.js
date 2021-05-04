d2d70e2b617dd935a5ed8fc8447e61f3bed3a96b
ArnaudDoubremelle <arnaud.doub@gmail.com>
2021-05-04 17:43:28 +0200

    test: add test for Heading.vue

---
+++ test/unit/Heading.spec.js
@@ -0,0 +1,11 @@
+import { mount } from '@vue/test-utils'
+import Heading from '@/components/Heading.vue'
+
+describe('Heading.vue', () => {
+  it('should display header text', () => {
+    const msg = 'This is a title with an h1 tag'
+    const wrapper = mount(Heading, { props: { size: 1 }, slots: { default: msg } })
+
+    expect(wrapper.find('h1').text()).toEqual(msg)
+  })
+})
