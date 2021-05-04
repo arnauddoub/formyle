9a098e8e6c25724f97000a2de1cfdb84b66bf3a7
ArnaudDoubremelle <arnaud.doub@gmail.com>
2021-05-04 19:17:20 +0200

    test: add test for format utils

--- src/utils/format.js
+++ src/utils/format.js
@@ -1,11 +1,23 @@
+/**
+ * @param {int} number
+ * @returns {string}
+ */
 export function formatEUR(number) {
   return readableNumber(number) + '\xa0€'
 }
 
+/**
+ * @param {int} number
+ * @returns {string}
+ */
 export function readableNumber(number) {
   return number ? number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ') : number
 }
 
+/**
+ * @param {string} str
+ * @returns {string}
+ */
 export function stripFirstZero(str) {
   return str.length >= 2 && str.charAt(0) === '0' ? str.substr(1) : str
 }
