de1a71be34377e5333ef6d393670ec4e3ef21a8a
ArnaudDoubremelle <arnaud.doub@gmail.com>
2021-05-11 23:39:05 +0200

    config: add vitejs library mode

--- vite.config.js
+++ vite.config.js
@@ -9,4 +9,22 @@ export default defineConfig({
       '@': resolve(__dirname, './src'),
     },
   },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.js'),
+      name: 'Formyle',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
 })
