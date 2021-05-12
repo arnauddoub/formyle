cb6cc98d43c035c9cbe380965aa556c681a533e1
ArnaudDoubremelle <arnaud.doub@gmail.com>
2021-05-12 18:08:51 +0200

    remove dependencies useless

--- src/main.js
+++ src/main.js
@@ -1,12 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './styles/main.css'
-import router from './router'
-import store from './store'
-import Maska from 'maska'
 
 const app = createApp(App)
-app.use(router)
-app.use(store)
-app.use(Maska)
 app.mount('#app')
