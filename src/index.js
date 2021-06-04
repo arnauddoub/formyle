08b7d4899849b1bdfb54de4b53930cbaa3f314bc
ArnaudDoubremelle <arnaud.doub@gmail.com>
2021-06-04 11:44:20 +0200

    fix style and name import

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ import RadioButton from './components/radio-button'
 import RadioGroup from './components/radio-group'
 import Selecta from './components/selecta'
 
+import './styles/main.css'
+
 const components = {
   BirthDate,
   Categories,
@@ -43,6 +45,6 @@ export { default as Heading } from './components/heading'
 export { default as InputNumeric } from './components/inputa'
 export { default as Inputa } from './components/input-numeric'
 export { default as Navigation } from './components/navigation'
-export { default as RadioButton } from './components/radio-buttons'
+export { default as RadioButton } from './components/radio-button'
 export { default as RadioGroup } from './components/radio-group'
 export { default as Selecta } from './components/selecta'
